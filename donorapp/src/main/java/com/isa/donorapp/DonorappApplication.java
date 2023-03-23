dff3e0349d33cbc3484a83e3812d5c93d29506b7
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2023-03-23 16:14:35 +0100

    feat: project initialization

---
+++ donorapp/src/main/java/com/isa/donorapp/DonorappApplication.java
@@ -0,0 +1,13 @@
+package com.isa.donorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonorappApplication.class, args);
+	}
+
+}
---
+++ donorapp/src/test/java/com/isa/donorapp/DonorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.donorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
