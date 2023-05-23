12f8bf1ed86a4efd1e59e2b7c9100399f45479a0
strahinjabogdanovic <bogdanovic.ra12.2018@uns.ac.rs>
2023-05-23 17:05:11 +0200

    feat: navbar

--- donorapp-frontend/src/app/app.module.ts
+++ donorapp-frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { UserUpcomingAppointmentsComponent } from './user-upcoming-appointments/
 import { UserQuestionnaireComponent } from './user-questionnaire/user-questionnaire.component';
 import { UserDonationCenterComponent } from './user-donation-center/user-donation-center.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     UserUpcomingAppointmentsComponent,
     UserQuestionnaireComponent,
     UserDonationCenterComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ donorapp-frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  public role: any;
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.role = localStorage.getItem('Role');
+    console.log(this.role);
+
+    if(this.role == "ROLE_USER"){
+      this.role = "user";
+    }
+    else if (this.role == "ROLE_ADMINISTRATOR"){
+      this.role = "admin";
+    }
+    else if (this.role == "ROLE_STAFF"){
+      this.role = "staff";
+    }
+    else {
+      this.role = " ";
+    }
+
+  }
+
+  signout(){
+    //localStorage.clear();
+  }
+
+}
