013b1f14809ecece090e77fb3301123fe7cb7810
boskoo <andrija.ancobosnjakovic@gmail.com>
2023-06-09 05:26:20 +0200

    feat: implement search and filter of centers

--- donorapp-frontend/src/app/user-homepage/user-homepage.component.ts
+++ donorapp-frontend/src/app/user-homepage/user-homepage.component.ts
@@ -6,58 +6,90 @@ import { UserHomepageInfo } from '../user-homepage-info';
 import { Role } from '../role';
 
 @Component({
-  selector: 'app-user-homepage',
-  templateUrl: './user-homepage.component.html',
-  styleUrls: ['./user-homepage.component.css']
+    selector: 'app-user-homepage',
+    templateUrl: './user-homepage.component.html',
+    styleUrls: ['./user-homepage.component.css'],
 })
-export class UserHomepageComponent implements OnInit{
-  penaltyCount: number = 0;
-  public centers = [] as DonationCenter[];
-  userInfo!: UserHomepageInfo;
-  errorMsg = "";
-  selectedSortOption = "name"
-  loadUserFailed = true;
+export class UserHomepageComponent implements OnInit {
+    penaltyCount: number = 0;
+    public centers = [] as DonationCenter[];
+    public centersSearchResult = [] as DonationCenter[];
+    userInfo!: UserHomepageInfo;
+    errorMsg = '';
+    selectedSortOption = 'name';
+    loadUserFailed = true;
+    searchField: string = '';
+    selectedFilterOption: string = '-1';
 
-  constructor(private router: Router, private _userHomepageService: UserHomepageService) { }
-  
-  ngOnInit(): void {
-    this.userInfo = new UserHomepageInfo("", "", "", 0);
+    constructor(private router: Router, private _userHomepageService: UserHomepageService) {}
 
-    this._userHomepageService.getAllDonationCenters().subscribe(data => {
-      this.centers = data;
-      this.sortCenters();
-    },
-      error => this.errorMsg = "Couldn't load centers");
+    ngOnInit(): void {
+        this.userInfo = new UserHomepageInfo('', '', '', 0);
 
-    let role = localStorage.getItem("Role");
-    if(role == Role.User)
-      this._userHomepageService.getUserInfo().subscribe(data => {
-        this.userInfo = data;
-        this.loadUserFailed = false;
-      },
-        error => this.loadUserFailed = true);
-    else
-        this.loadUserFailed = true;
-    
-  }
+        this._userHomepageService.getAllDonationCenters().subscribe(
+            (data) => {
+                this.centers = data;
+                this.centersSearchResult = data;
+                this.sortCenters();
+            },
+            (error) => (this.errorMsg = "Couldn't load centers")
+        );
 
-  sortCenters(): void {
-    switch (this.selectedSortOption) {
-      case 'name':
-        this.centers.sort((a, b) => a.name.localeCompare(b.name));
-        break;
-      case 'rating':
-        this.centers.sort((a, b) => b.rating - a.rating);
-        break;
-      case 'city':
-        this.centers.sort((a, b) => a.city.localeCompare(b.city));
-        break;
-      default:
-        break;
+        let role = localStorage.getItem('Role');
+        if (role == Role.User)
+            this._userHomepageService.getUserInfo().subscribe(
+                (data) => {
+                    this.userInfo = data;
+                    this.loadUserFailed = false;
+                },
+                (error) => (this.loadUserFailed = true)
+            );
+        else this.loadUserFailed = true;
     }
-  }
 
-  openCenter(id: number): void {
-    this.router.navigate(['user-donation-center/' + id]);
-  } 
+    searchFilterAndSort() {
+        this.centersSearchResult = [...this.centers];
+        this.searchCenters();
+        this.filterCenters();
+        this.sortCenters();
+    }
+
+    searchCenters() {
+        if (this.searchField.trim() !== '') {
+            this.centersSearchResult = this.centersSearchResult.filter(
+                (center) =>
+                    center.name.toLowerCase().includes(this.searchField.toLowerCase()) ||
+                    center.city.toLowerCase().includes(this.searchField.toLowerCase()) ||
+                    center.address.toLowerCase().includes(this.searchField.toLowerCase())
+            );
+        }
+    }
+
+    filterCenters() {
+        if (this.selectedFilterOption !== '-1') {
+            this.centersSearchResult = this.centersSearchResult.filter(
+                (center) => center.rating > Number(this.selectedFilterOption)
+            );
+        }
+    }
+
+    sortCenters(): void {
+        switch (this.selectedSortOption) {
+            case 'name':
+                this.centersSearchResult.sort((a, b) => a.name.localeCompare(b.name));
+                break;
+            case 'rating':
+                this.centersSearchResult.sort((a, b) => b.rating - a.rating);
+                break;
+            case 'city':
+                this.centersSearchResult.sort((a, b) => a.city.localeCompare(b.city));
+                break;
+            default:
+                break;
+        }
+    }
+
+    openCenter(id: number): void {
+        this.router.navigate(['user-donation-center/' + id]);
+    }
 }
