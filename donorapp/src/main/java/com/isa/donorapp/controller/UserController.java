4816870f1b25c9d3d02229cadd6fbc9c6ca1ec8d
igorra1212018 <petrovic.ra121.2018@uns.ac.rs>
2023-06-13 20:04:46 +0200

    fix: uncommented function

--- donorapp/src/main/java/com/isa/donorapp/controller/UserController.java
+++ donorapp/src/main/java/com/isa/donorapp/controller/UserController.java
@@ -98,6 +98,29 @@ public class UserController {
 		}
 	}
 	*/
+	
+	@GetMapping("/allUsers")
+	@Secured("ROLE_STAFF")
+	public ResponseEntity<List<UserHomepageDTO>> getAllUsersList() {
+		
+		try {
+			List<User> users = userService.findAll();
+			List<UserHomepageDTO> result = new ArrayList<UserHomepageDTO>();
+			if (users.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			else {
+				for (User u : users) {
+					for (Role r : u.getRoles())
+					    if (r.getName().toString().equals("USER"))
+					    	result.add(new UserHomepageDTO(u.getEmail(), u.getFirstName(), u.getLastName(), u.getPenaltyCount()));
+				}
+				return new ResponseEntity<>(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	@PostMapping("/register")
 	public ResponseEntity<String> register(@RequestBody UserRegisterDTO signUpRequest, HttpServletRequest request)
